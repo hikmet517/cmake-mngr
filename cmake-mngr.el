a586d54b66a8586249d278b9b665ea460b70ebf5
Hikmet Altıntaş <hikmet1517@gmail.com>
2022-08-02 19:40:55 +0300

    fix url, add license

--- cmake-mngr.el
+++ cmake-mngr.el
@@ -1,12 +1,30 @@
 ;;; cmake-mngr.el --- Manage cmake projects -*- lexical-binding: t -*-
 
+;; Copyright (C) 2020-2022 Hikmet Altıntaş
+
 ;; Author: Hikmet Altıntaş (hikmet1517@gmail.com)
+;; Maintainer: Hikmet Altıntaş (hikmet1517@gmail.com)
+;; Created: 29 Aug 2020
 ;; Keywords: tools, extensions
-;; URL: "https://github.com/hikmet517/cmake-mngr.el"
+;; URL: https://github.com/hikmet517/cmake-mngr.el
 ;; Version: 0.1
+;; Package-Requires: ((seq "2.23") (tabulated-list "1.0"))
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 ;;; Commentary:
 ;; A convenient interface to cmake commands.
-
 
 ;;; TODO:
 ;; suggestions using cmake vars' types
